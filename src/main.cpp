841497e3d7536c6e62a5b142a7746c99682d99b0
Gourika J S <gourikasreenivas@gmail.com>
2025-12-26 16:15:22 +0530

    Handle Concurrent Clients

--- src/main.cpp
+++ src/main.cpp
@@ -27,72 +27,26 @@ void handle_client(int client_fd) {
 
 
 int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
-  
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+  int main(int argc, char **argv) {
+    // socket(), setsockopt(), bind(), listen() code above
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
 
-  // Uncomment the code below to pass the first stage
-  while (true) {
-    int client_fd = accept(server_fd,
-        (struct sockaddr*)&client_addr,
-        (socklen_t*)&client_addr_len);
+    while (true) {
+        int client_fd = accept(
+            server_fd,
+            (struct sockaddr*)&client_addr,
+            (socklen_t*)&client_addr_len
+        );
 
-    std::thread t(handle_client, client_fd);
-    t.detach();  // let thread run independently
-}
-  
-  char buffer[1024];
-
-while (true) {
-    int bytes_read = read(client_fd, buffer, sizeof(buffer));
-    if (bytes_read <= 0) {
-        break; // client disconnected
+        std::thread t(handle_client, client_fd);
+        t.detach();
     }
 
-    const char* response = "+PONG\r\n";
-    send(client_fd, response, strlen(response), 0);
+    return 0; // never reached
 }
 
-  close(client_fd);
-
-  close(server_fd);
 
   return 0;
 }
